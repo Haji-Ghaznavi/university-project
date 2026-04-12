4c752dcb6b442c7865172c32c407e24882f0cd3b
Samiullah Hussaini <samiullahhussaini313@gmail.com>
2026-04-12 17:11:29 +0430

    initial commit

---
+++ src/layouts/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="w-100 h-100 d-flex align-center justify-space-between">
+    <!-- 👉 Footer: left content -->
+    <span
+      class="d-flex align-center"
+      style="direction: rtl"
+    >
+      &copy;
+      {{ new Date().getFullYear() }} 
+      توسعه توسط شرکت تکنالوژی
+
+      <a
+        href="tel:+93745145758"
+        class="ms-3 mb-1"
+      >Company Name
+        <VIcon
+          style="direction: rtl"
+          icon="mdi-phone"
+        /></a>
+    </span>
+  </div>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,19 @@
+
+<template>
+  <VRow class="match-height">
+ 
+  </VRow>
+</template>
+
+<script setup>
+
+
+</script>
+
+
+
+<route lang="yaml">
+meta:
+  auth: true
+</route>
+
