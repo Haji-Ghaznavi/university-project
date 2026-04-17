86f5eccf6c43c2fd7cfbb13170b31194ba9ba28f
Samiullah Hussaini <samiullahhussaini313@gmail.com>
2026-04-17 20:52:01 +0430

    new changes

--- src/components/commons/BreadCrumbs.vue
+++ src/components/commons/BreadCrumbs.vue
@@ -4,14 +4,14 @@
       class="pe-2"
     >
       <v-card-title class="d-flex align-center justify-space-between text-sm pa-0">
-        <v-breadcrumbs :items="props.items">
-          <template v-slot:prepend>
-            <v-icon
-              icon="mdi-home"
-              size="small"
-            ></v-icon>
-          </template>
-        </v-breadcrumbs>
+          <v-breadcrumbs :items="props.items">
+            <template v-slot:prepend>
+              <v-icon
+                icon="mdi-home"
+                size="small"
+              ></v-icon>
+            </template>
+          </v-breadcrumbs>
         <v-btn @click="emit('onCreate')" class="float-start" width="110" height="35"> <v-icon class="pe-2" size="25">mdi-plus</v-icon>ایجاد کردن</v-btn>
       </v-card-title>
     </v-card>
