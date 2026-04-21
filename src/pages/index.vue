48d0db57348ef94a1e60f82ba0f326e6e2af9b60
zakariafanos6-dotcomz <zakariafanos6@gmail.com>
2026-04-21 23:51:32 +0430

    new change

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,4 +1,4 @@
 <template>
   <h1>this is the dashboard page</h1>
-  <p>All people can see the result of university projects</p>
+  <p>All people can see the result of university projects in here</p>
 </template>
